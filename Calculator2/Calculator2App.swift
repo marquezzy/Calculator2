574d90a33de354aa113566381b50ad420a0fd2b5
Mark Gallegos <mark.gallegos@omadahealth.com>
2021-02-16 10:14:53 -0800

    Initial Commit

---
+++ Calculator2/Calculator2App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator2App.swift
+//  Calculator2
+//
+//  Created by mark.gallegos on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Calculator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
